f3b61ed2612f237a3ee9658fdb5f894e5e37bc96
erki <erkiraak@gmail.com>
2022-06-04 14:34:07 +0300

    Added SQL

--- 6_SQL/SQL.py
+++ 6_SQL/SQL.py
@@ -1,6 +1,7 @@
 from mysql.connector import Error, connect
+from os import environ
 
-PASSWORD = "P4roolP4rool"
+PASSWORD = environ.get("SQL_PASS")
 
 
 def create_database(database_name):
@@ -158,10 +159,10 @@ data = {
 # for database, table in tables.items():
 #     for table_name, _ in table.items():
 #         drop_table(database, table_name)
-
-while tables := show_tables("cinematic"):
-    for table in tables:
-        drop_table("cinematic", table)
+#
+# while tables := show_tables("cinematic"):
+#     for table in tables:
+#         drop_table("cinematic", table)
 
 # sql_query(select_query_2002)
 # sql_query("select * from movies left join directors on movies.movie_director_id = directors.director_id")
