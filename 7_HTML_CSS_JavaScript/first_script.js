c9d219b7a6594ebe89246dc044324c3bc2d6a5b3
erki <erkiraak@gmail.com>
2022-07-03 11:37:46 +0300

    Added HTML and CSS

---
+++ 7_HTML_CSS_JavaScript/first_script.js
@@ -0,0 +1,51 @@
+for (let i = 0; i < 10; i ++){
+    console.log(i)
+}
+// let myVariable = "this is a string" // this is a variable
+// const myConstant = 1 // this is a constant
+//
+// let numberOne, numberTwo
+//
+// numberOne = 420
+// numberTwo = 69
+//
+// let result = numberOne + numberTwo
+//
+// // "for" loop +
+// // ("do" +) "while" loop +
+//
+// // "for + in" loop
+// // "for + of" loop
+//
+// let condition = 5
+//
+// for (let i = 0; i < 10; i++) {
+//     console.log("Loop iterator: " + i)
+// }
+//
+// do {
+//     console.log("This is 'do' expression")
+//     console.log("Condition value: " + condition)
+//     condition++
+// }
+// while (condition < 10)
+//
+// // iterator or index
+// // Loop safety - a chance to get into an infinity loop
+// // While loop
+// // "==" - it means that we do not count in the type of the variables
+// // 5 === "5" - this expression IS NOT equal (=== also checks the type of the variable)
+// // 5 == "5" - this expression IS equal
+//
+//
+//
+// // "if" statement
+// let referenceNumber = 500
+//
+// if (result < referenceNumber) {
+//     console.log("The result is less than 500")
+// } else if (result > referenceNumber) {
+//     console.log("The result is less than 500")
+// } else {
+//     console.log("Some error occurred")
+// }
